65688bbcbc8f2e3945b7acf8f6ed9406731470e0
Addison Shuppy <addyshuppy@gmail.com>
2019-05-07 14:59:43 -0600

    Scripts
    Added an example script so Kyle can know how to get coding.

---
+++ Sanbox/Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, Kyle!  I am the tree on the top left!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
