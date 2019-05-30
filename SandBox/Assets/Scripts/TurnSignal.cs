96121c9ab39a7e6319578c73ff03750cc8ec35c8
Addison Shuppy <addyshuppy@gmail.com>
2019-05-30 15:30:01 -0600

    Attempted Enemy AI
    attemtped to make Enemy AI based on what KSmithey and I did last night.

---
+++ SandBox/Assets/Scripts/TurnSignal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnSignal : MonoBehaviour
+{
+
+    [SerializeField]
+    private int direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetDirection()
+    {
+        return direction;
+    }
+}
