69b02102e35718aeac7225ab5f18b0a72b099e9f
Addison Shuppy <addyshuppy@gmail.com>
2019-10-11 19:36:03 -0600

    Cameras and Lv1
    Messed with the minimap and cameras so that the code for the minimap could work in both level 1 and the tutorial.  So far, it's not done.

--- SandBox/Assets/Scripts/UI/MinimapIcon.cs
+++ SandBox/Assets/Scripts/UI/MinimapIcon.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class MinimapIcon : MonoBehaviour
 {
-    [SerializeField] public GameObject symbolizedObject;
+    [SerializeField] public GameObject symbolizedObject, mainCamera;
     private Vector3 position;
     private int spacing;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        mainCamera = GameObject.Find("MainCamera");
         spacing = 11;
         UpdatePosition();
     }
@@ -32,11 +32,24 @@ public class MinimapIcon : MonoBehaviour
     {
         if (symbolizedObject != null)
         {
-            position = symbolizedObject.transform.position;
-            position.x += 26.5f;
-            position.y += 12;
-            position.x = 5.7f * position.x;
-            position.y = 6.2f * position.y;
+            float mapSizeX = transform.parent.GetComponent<RectTransform>().rect.width;
+            float mapSizeY = transform.parent.GetComponent<RectTransform>().rect.height;
+            float MainGuyPositionX = symbolizedObject.transform.position.x - (1.42f * mainCamera.GetComponent<CameraManager>().xMin); //35.5f;
+            float MainGuyPositionY = symbolizedObject.transform.position.y - (1.54f * mainCamera.GetComponent<CameraManager>().yMin); //17.5f;
+            float CameraRangeX = mainCamera.GetComponent<CameraManager>().xMax - mainCamera.GetComponent<CameraManager>().xMin;
+            float CameraRangeY = mainCamera.GetComponent<CameraManager>().yMax - mainCamera.GetComponent<CameraManager>().yMin;
+            position.x = mapSizeX 
+                * MainGuyPositionX 
+                / (CameraRangeX);
+            position.y = mapSizeY
+                * MainGuyPositionY
+                / (CameraRangeY -3);
+            //float horizConstant = mainCamera.GetComponent<CameraManager>().x
+            //need to find some value that changes when the screen is resized.
+            //position.x += 26.5f;
+            //position.y += 12;
+            //position.x = 5.7f * position.x;
+            //position.y = 6.2f * position.y;
             transform.position = position;
         } else
         {
